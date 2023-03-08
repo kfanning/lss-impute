4d8722a6c5861826c81610d4ba9dc0d3dd805098
kfanning <kfanning@users.noreply.github.com>
2023-03-08 16:54:43 -0500

    Added functions to cat.py for generic mask downsampling and applying it to downsampling randoms by NTILE

--- py/lssimpute/cat.py
+++ py/lssimpute/cat.py
@@ -34,4 +34,45 @@ def split_obs_missed(fullcat, compcat, region='N'):
     observ = fullreg[fullreg['ZWARN'] == 0]
     return observ, missed
 
+def downsample_mask(base_mask, fraction, seed=None):
+    '''
+    Randomly re-adds a fraction of the masked values
+
+    Inputs:
+        base_mask - np.array like with binary (True/False or 1/0) values
+        fraction - float fraction of masked values (0) to unmask (change to 1)
+    Out:
+        new mask array (np.array) with fewer masked (0) values but same length
+    '''
+    masked_idx, _ = np.where(base_mask == 0)
+    unmask_num = int(len(masked_idx) * fraction) #just get an int close to faction
+    # setup rng shuffle to not require that source was preshuffled
+    rng = np.random.default_rng(seed=seed)
+    rng.shuffle(masked_idx)
+    # unmask_num of len(masked_idx) will be unmasked
+    unmask_idx = masked_idx[:unmask_num]
+    base_mask[unmask_idx] = 1
+    return base_mask
+
+def downsample_randoms_by_ntile(randcat, fraction_by_ntile):
+    '''
+    Downsamples random catalog by given amount based on given faction
+    to keep per NTILE value
+
+    Inputs:
+        randcat - astropy.Table like random galaxy catalog
+        fraction_by_ntile - list like iterable with fraction of randoms to keep
+                            per NTILE ordered from NTILE = 1 to NTILE = len()
+    Out:
+        Downsampled random catalog
+    '''
+    tot_mask = np.ones(len(randcat))
+    for ntile, frac in enumerate(fraction_by_ntile, start=1):
+        # mask out values at ntiles
+        new_mask = np.array(randcat['NTILE']!=ntile)
+        # downsample_mask removes frac of masked values
+        downsampled = downsample_mask(new_mask, frac)
+        # apply mask to intialized total mask
+        tot_mask = tot_mask & downsampled
+    return randcat[tot_mask] 
 
