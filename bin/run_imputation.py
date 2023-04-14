b836fd49aa7842b48dae98f978b7e1b67a6190d6
kfanning <kfanning@users.noreply.github.com>
2023-04-14 14:34:23 -0400

    Saving intermediate files

--- bin/run_imputation.py
+++ bin/run_imputation.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from astropy.table import Table, vstack
 from lssimpute import dirs, cat, impute
+from LSS.tabulated_cosmo import TabulatedDESI
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--tracer', '-t', default='LRG', help='Which LSS tracer catalogs to run on (LRG, ELG, QSO)')
@@ -35,13 +36,35 @@ full =  Table.read(os.path.join(catdir, f'{uargs.tracer}_full.dat.fits'))
 obsn, missn = cat.split_obs_missed(full, clusn, region='N')
 obss, misss = cat.split_obs_missed(full, cluss, region='S')
 
-#merge both obs cause why not (maybe not idk for now)
-obs = vstack([obsn, obss])
-
 #get nn cats, currently only extracting nn, not some set of them
 mis_nncat_n = cat.extract_nn(missn, obs)
 mis_nncat_s = cat.extract_nn(misss, obs)
-obs_nncat = cat.extract_nn(obs)
+obs_nncat_n = cat.extract_nn(obsn)
+obs_nncat_s = cat.extract_nn(obss)
+
+# Add physical units
+cosmo = TabulatedDESI()
+mis_nncat_n['R'] = cosmo.comoving_radial_distance(mis_nncat_n['Z'])
+mis_nncat_n['r_n0'] = cosmo.comoving_radial_distance(mis_nncat_n['z_n0'])
+mis_nncat_n['sperp_n0'] = mis_nncat_n['r_n0']*mis_nncat_n['angdist_n0'] #small angle aprox + angdist in rad!
+obs_nncat_n['r_n0'] = cosmo.comoving_radial_distance(obs_nncat_n['z_n0'])
+obs_nncat_n['sperp_n0'] = obs_nncat_n['r_n0']*obs_nncat_n['angdist_n0'] #small angle aprox + angdist in rad!
+obs_nncat_n['R'] = cosmo.comoving_radial_distance(obs_nncat_n['Z'])
+
+mis_nncat_s['R'] = cosmo.comoving_radial_distance(mis_nncat_s['Z'])
+mis_nncat_s['r_n0'] = cosmo.comoving_radial_distance(mis_nncat_s['z_n0'])
+mis_nncat_s['sperp_n0'] = mis_nncat_s['r_n0']*mis_nncat_s['angdist_n0'] #small angle aprox + angdist in rad!
+obs_nncat_s['r_n0'] = cosmo.comoving_radial_distance(obs_nncat_s['z_n0'])
+obs_nncat_s['sperp_n0'] = obs_nncat_s['r_n0']*obs_nncat_s['angdist_n0'] #small angle aprox + angdist in rad!
+obs_nncat_s['R'] = cosmo.comoving_radial_distance(obs_nncat_s['Z'])
+
+mis_nncat_n.write(os.path.join(stagedir, f'{uargs.tracer}_N_missing.dat.fits'), overwrite=uargs.overwrite)
+mis_nncat_s.write(os.path.join(stagedir, f'{uargs.tracer}_S_missing.dat.fits'), overwrite=uargs.overwrite)
+obs_nncat_n.write(os.path.join(stagedir, f'{uargs.tracer}_N_clustering.dat.fits'), overwrite=uargs.overwrite)
+obs_nncat_s.write(os.path.join(stagedir, f'{uargs.tracer}_S_clustering.dat.fits'), overwrite=uargs.overwrite)
+
+#merge both obs cause why not (maybe not idk for now)
+obs_nncat = vstack([obs_nncat_n, obs_nncat_s])
 
 #run imputation
 impn = impute.ImputeModel(obs_nncat, mis_nncat_n)
