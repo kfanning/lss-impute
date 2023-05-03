4093c404e82fd2fb035e91837f52431ac5a73bad
kfanning <kfanning@users.noreply.github.com>
2023-05-02 20:07:40 -0400

    Added stitch_impute script to stitch together real mock catalogs with imputed catalog into single catalog

---
+++ bin/stitch_impute.py
@@ -0,0 +1,38 @@
+import os
+import argparse
+from astropy.table import Table, vstack
+from lssimpute import dirs, cat
+from LSS.tabulated_cosmo import TabulatedDESI
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--tracer', '-t', default='LRG', help='Which LSS tracer catalogs to run on (LRG, ELG, QSO)')
+parser.add_argument('--survey', '-s', default='y1mock', help='Survey to use (typically main or y1mock)')
+parser.add_argument('--version', '-v', default=0, help='catalog version, for mocks this is mock number')
+parser.add_argument('--overwrite', '-o', action='store_true', help='Set flag to allow overwriting of existing files')
+parser.add_argument('--impversion', '-i', default=None, help='override version for imputation, default None == same as version')
+
+# add dir management
+# catdir (for base catalogs for reading, no writing)
+# temp dir (intermediate files like logging, nn cats, etc)
+# outdir (for imputed cats)
+
+uargs = parser.parse_args()
+catdir = dirs.get_catdir(uargs.survey, uargs.version)
+if uargs.impversion is not None:
+    impver = uargs.impversion
+else:
+    impver = uargs.version
+stagedir = dirs.get_stagedir(uargs.survey, impver)
+impute_dir = dirs.get_catdir('y1model', impver)
+stitch_dir = dirs.get_catdir('y1impute', impver)
+
+#read catalogs (using complete and full catalogs then splitting later)
+clusn = Table.read(os.path.join(catdir, f'{uargs.tracer}_N_clustering.dat.fits'))
+cluss = Table.read(os.path.join(catdir, f'{uargs.tracer}_S_clustering.dat.fits'))
+impn = Table.read(os.path.join(impute_dir, f'{uargs.tracer}_N_clustering.dat.fits'))
+imps = Table.read(os.path.join(impute_dir, f'{uargs.tracer}_S_clustering.dat.fits'))
+
+stitchedn = vstack([clusn, impn], join_type='inner')
+stitchedn.write(os.path.join(stitch_dir, f'{uargs.tracer}_N_clustering.dat.fits'))
+stitcheds = vstack([cluss, imps], join_type='inner')
+stitcheds.write(os.path.join(stitch_dir, f'{uargs.tracer}_S_clustering.dat.fits'))
