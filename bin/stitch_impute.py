34ea152e234511bd6990eeb2eb746fe7f896b3b6
kfanning <kfanning@users.noreply.github.com>
2023-06-09 16:20:10 -0400

    Added option to cut stitched together catalogs to zrange

--- bin/stitch_impute.py
+++ bin/stitch_impute.py
@@ -10,11 +10,14 @@ parser.add_argument('--survey', '-s', default='y1mock', help='Survey to use (typ
 parser.add_argument('--version', '-v', default=0, help='catalog version, for mocks this is mock number')
 parser.add_argument('--overwrite', '-o', action='store_true', help='Set flag to allow overwriting of existing files')
 parser.add_argument('--impversion', '-i', default=None, help='override version for imputation, default None == same as version')
+parser.add_argument('--cut', '-c', action='store_true', help='Set flag to cut stiched together catalog to redshift range')
 
 # add dir management
 # catdir (for base catalogs for reading, no writing)
 # temp dir (intermediate files like logging, nn cats, etc)
 # outdir (for imputed cats)
+
+zrange = {'LRG':[0.4,1.1], 'ELG':[0.8,1.6]}
 
 uargs = parser.parse_args()
 catdir = dirs.get_catdir(uargs.survey, uargs.version)
@@ -33,6 +36,10 @@ impn = Table.read(os.path.join(impute_dir, f'{uargs.tracer}_N_clustering.dat.fit
 imps = Table.read(os.path.join(impute_dir, f'{uargs.tracer}_S_clustering.dat.fits'))
 
 stitchedn = vstack([clusn, impn], join_type='inner')
+if uargs.cut:
+    stichedn = stichedn[(stitchedn['Z'] > zrange[uargs.tracer][0]) & (stitchedn['Z'] < zrange[uargs.tracer][1])]
 stitchedn.write(os.path.join(stitch_dir, f'{uargs.tracer}_N_clustering.dat.fits'))
 stitcheds = vstack([cluss, imps], join_type='inner')
+if uargs.cut:
+    sticheds = sticheds[(stitcheds['Z'] > zrange[uargs.tracer][0]) & (stitcheds['Z'] < zrange[uargs.tracer][1])]
 stitcheds.write(os.path.join(stitch_dir, f'{uargs.tracer}_S_clustering.dat.fits'))
